60137136747491c7cbf006c673e79ecaddf04521
mounika <mounika@DESKTOP-5O6JPS8>
2019-02-27 18:30:24 +0530

    my first java multi prgm

---
+++ src/mypack/Multipgm.java
@@ -0,0 +1,17 @@
+package mypack;
+
+public class Multipgm {
+
+	public static void main(String[] args) 
+	{
+		int num1=8;
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(num1+"x"+i+"="+(num1*i));
+			
+		}
+
+
+	}
+
+}
